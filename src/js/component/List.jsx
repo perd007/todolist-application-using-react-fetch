c8dd0e4660622ea10d7628378edebed8c056f569
jose carlos perdomo herrera <phpperdomo@gmail.com>
2023-07-31 17:00:30 -0400

    subiendo todo

---
+++ src/js/component/List.jsx
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from "react";
+
+const api="https://express-blog-xa7v.onrender.com/todo/users/perd007";
+
+const tarea ={
+    label:"",
+    done:true
+}
+const List = () => {
+
+    const [todolist, setTodoList] = useState([tarea]);
+    
+    const getTodoListApi= async () => {
+            const response= await fetch(api);
+            const data = await response.json();
+            setTodoList(data)
+          console.log(data);
+
+    }
+
+const updateApi = async  (array)=>{
+    //actualizamos la api pasando la lista actualizada todolist
+    const response= await fetch(api,{
+        method: 'PUT',
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(array)
+    });
+    const data = await response.json(); 
+    //console.log(data);
+    if(response.ok){
+        getTodoListApi();
+    }  
+}
+
+    const handleChange =(event) => {
+        if (event.key === "Enter") {
+            //agregamos los valores de la lista a newList
+            const NewList = [...todolist,{ [event.target.name]: event.target.value, done: true}];
+            //actualizamos la api
+            updateApi(NewList);
+            //limpiamos el input          
+            event.target.value = "";
+            
+        }
+    }
+
+    //eliminamos el elemento seleccionado
+    const deleteList = async (key) => {
+        const updateTodoList = todolist.filter((element, index) => index !== key);
+        updateApi(updateTodoList);
+        
+    }
+
+    //cargamos la api al inicio
+    useEffect(()=>{
+        getTodoListApi();
+    },[])
+       
+    
+
+    return (
+        <>
+            <div className="input-group mb-3">
+                <input type="text" name="label" onKeyDown={handleChange} className="form-control" />
+            </div>
+            <ul className="list-group">
+
+                {todolist.map((element, index) => {
+                    return (
+                        <li className="list-group-item d-flex justify-content-between" key={index} >
+                            {element.label}
+                            <button type="button" onClick={() => deleteList(index)} className="btn-close" aria-label="Close"></button></li>
+                    );
+                })
+                }
+        <li className="list-group-item d-flex justify-content-between" >{todolist.length} Items</li>
+            </ul>
+
+        </>
+    );
+}
+export default List;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import List from "./List";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -8,12 +9,7 @@ const Home = () => {
 	return (
 		<div className="text-center">
 			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
+			<List/>
 			<p>
 				Made by{" "}
 				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
